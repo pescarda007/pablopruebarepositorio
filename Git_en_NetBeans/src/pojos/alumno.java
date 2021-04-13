b222b4238ae04c379394ba2471cb4edbd9101b7b
pescarda007 <pescarda007@gmail.com>
2021-04-13 11:41:42 +0200

    Subida 1

---
+++ Git_en_NetBeans/src/pojos/alumno.java
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pojos;
+
+/**
+ *
+ * @author esccalpa
+ */
+public class alumno {
+    
+   private int numAl;
+   private String nombre;
+   private String apellido;
+
+    public alumno(int numAl, String nombre, String apellido) {
+        this.numAl = numAl;
+        this.nombre = nombre;
+        this.apellido = apellido;
+    }
+
+    public int getNumAl() {
+        return numAl;
+    }
+
+    public void setNumAl(int numAl) {
+        this.numAl = numAl;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    @Override
+    public String toString() {
+        return "alumno{" + "numAl=" + numAl + ", nombre=" + nombre + ", apellido=" + apellido + '}';
+    }
+    
+    
+}
---
+++ Git_en_NetBeans/src/pruebagit/pruebasgit.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pruebagit;
+
+import pojos.alumno;
+
+/**
+ *
+ * @author esccalpa
+ */
+public class pruebasgit {
+    
+    public static void main(String[] args) {
+        
+        alumno al = new alumno(1, "Pablo", "Escarda");
+        
+        
+    }
+    
+    
+}
