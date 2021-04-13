a865cd521ee94928307894cb1ba1134e66077b11
pescarda007 <pescarda007@gmail.com>
2021-04-13 12:01:27 +0200

    Subida 3

---
+++ Git_en_NetBeans/src/pojos/Curso.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pojos;
+
+/**
+ *
+ * @author esccalpa
+ */
+public class Curso {
+    
+    private int idCurso;
+    private String nombre;
+
+    public Curso() {
+    }
+
+    public int getIdCurso() {
+        return idCurso;
+    }
+
+    public void setIdCurso(int idCurso) {
+        this.idCurso = idCurso;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    
+    
+    
+}
